1d8447cb9032d95679fc26095dff4be3bd28be8c
Angie Cruz <acruz@dchanges.com>
2020-09-15 18:30:30 -0500

    Se agregan estados

--- src/components/BadgeForm.js
+++ src/components/BadgeForm.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class BadgeForm extends React.Component {
+    state = {
+        firstName: "Angie"
+    };
+
     handleChange = (e) => {
-        console.log({
-            name: e.target.name,
-            value: e.target.value,
+        /*         console.log({
+                    name: e.target.name,
+                    value: e.target.value,
+                }) */
+        this.setState({
+            [e.target.name]: e.target.value,
         })
     };
 
@@ -15,22 +22,78 @@ class BadgeForm extends React.Component {
     handleSubmit = e => {
         e.preventDefault();
         console.log("Form was submitted");
+        console.log(this.state);
     };
 
     render() {
         return (
-           <div>
-               <h1>New Attendant</h1>
+            <div>
+                <h1>New Attendant</h1>
 
-               <form>
-                   <div className="form-group">
+                <form>
+                    <div className="form-group">
                         <label>First Name</label>
-                        <input onChange={this.handleChange} className="form-control" type="text" name="firstName" />
-                   </div>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="firstName"
+                            value={this.state.firstName}
+                        />
+                    </div>
 
-                   <button onClick={this.handleClick} className="btn btn-primary">Save</button>
-               </form>
-           </div>
+                    <div className="form-group">
+                        <label>Last Name</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="lastName"
+                            value={this.state.lastName}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Email</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="email"
+                            name="email"
+                            value={this.state.email}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Job Title</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="jobTitle"
+                            value={this.state.jobtitle}
+                        />
+                    </div>
+
+                    <div className="form-group">
+                        <label>Twitter</label>
+                        <input
+                            onChange={this.handleChange}
+                            className="form-control"
+                            type="text"
+                            name="twitter"
+                            value={this.state.twitter}
+                        />
+                    </div>
+
+                    <button
+                        type="submit"
+                        onClick={this.handleClick}
+                        className="btn btn-primary">
+                        Save
+                    </button>
+                </form>
+            </div>
         )
     }
 };
