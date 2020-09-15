5f434d5e8faf870414edea38d68292c1119a5a68
Angie Cruz <acruz@dchanges.com>
2020-09-15 17:49:22 -0500

    Se enlazan eventos

--- src/pages/BadgeNew.js
+++ src/pages/BadgeNew.js
@@ -2,28 +2,33 @@ import React from 'react';
 
 import './styles/BadgeNew.css';
 import header from '../images/badge-header.svg';
-import Navbar from '../components/Navbar.js';
+import Navbar from '../components/Navbar';
 import Badge from '../components/Badge';
+import BadgeForm from '../components/BadgeForm';
 
 class BAdgeNew extends React.Component {
     render() {
-        return(
+        return (
             <div>
                 <Navbar />
                 <div className="BadgeNew__hero">
                     <img className="img-fluid" src={header} alt="Logo" />
                 </div>
-                
+
                 <div className="container">
                     <div className="row">
-                        <div className="col">
-                            <Badge 
-                            firstName="Angie" 
-                            lastName="Cruz" 
-                            twitter="nn" 
-                            jobtitle="Frontend Student"
-                            avatarUrl="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
+                        <div className="col-6">
+                            <Badge
+                                firstName="Angie"
+                                lastName="Cruz"
+                                twitter="nn"
+                                jobtitle="Frontend Student"
+                                avatarUrl="https://www.gravatar.com/avatar/205e460b479e2e5b48aec07710c08d50?f=y"
                             />
+                        </div>
+
+                        <div className="col-6">
+                            <BadgeForm />
                         </div>
                     </div>
 
