a50234550b08578679167bdd370ee965ad22ee96
Angie Cruz <acruz@dchanges.com>
2020-09-17 18:31:24 -0500

    Se agregan los modales para eliminar badges

--- src/pages/BadgeDetailsContainer.js
+++ src/pages/BadgeDetailsContainer.js
@@ -1,9 +1,8 @@
 import React from 'react';
 
-import './styles/BadgeDetails.css';
+import BadgeDetails from './BadgeDetails';
 import PageLoading from '../components/PageLoading';
 import PageError from '../components/PageError';
-import BadgeDetails from './BadgeDetails';
 import api from '../api';
 
 class BadgeDetailsContainer extends React.Component {
@@ -11,6 +10,7 @@ class BadgeDetailsContainer extends React.Component {
     loading: true,
     error: null,
     data: undefined,
+    modalIsOpen: false,
   };
 
   componentDidMount() {
@@ -28,6 +28,27 @@ class BadgeDetailsContainer extends React.Component {
     }
   };
 
+  handleOpenModal = e => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  handleCloseModal = e => {
+    this.setState({ modalIsOpen: false });
+  };
+
+  handleDeleteBadge = async e => {
+    this.setState({ loading: true, error: null });
+
+    try {
+      await api.badges.remove(this.props.match.params.badgeId);
+      this.setState({ loading: false });
+
+      this.props.history.push('/badges');
+    } catch (error) {
+      this.setState({ loading: false, error: error });
+    }
+  };
+
   render() {
     if (this.state.loading) {
       return <PageLoading />;
@@ -39,6 +60,10 @@ class BadgeDetailsContainer extends React.Component {
 
     return (
       <BadgeDetails
+        onCloseModal={this.handleCloseModal}
+        onOpenModal={this.handleOpenModal}
+        modalIsOpen={this.state.modalIsOpen}
+        onDeleteBadge={this.handleDeleteBadge}
         badge={this.state.data}
       />
     );
